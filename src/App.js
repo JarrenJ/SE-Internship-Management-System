1a34c0bf6241e42b194359e6a9d97d8b83d6442f
Jarren Jackson <j.jarren@yahoo.com>
2021-02-16 13:32:16 -0600

    Initial barebones project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from "./components/Home/Home";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+        {/*<Navbar />*/}
+        <Switch>
+            <Route path='/' exact component={Home} />
+            {/*<Route component={error404} />*/}
+        </Switch>
+        {/*<Footer />*/}
+    </Router>
   );
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../../App.css';
+
+
+export default function Home() {
+    return(
+        <div className="App">
+            <div className="App-header">
+                <p>Hello World!</p>
+            </div>
+        </div>
+    )
+}
