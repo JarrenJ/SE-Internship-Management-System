ee61c1933520f617a6b159d7a457e052f898fdbf
Jarren Jackson <j.jarren@yahoo.com>
2021-02-19 05:09:44 -0600

    Directory restructuring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Home from "./components/Home/Home";
+import Home from "./pages/Home";
 import './App.css';
 
 function App() {
