08c515e9925f721c3c23586baeff9ee0d585c298
JarrenJ <j.jarren@yahoo.com>
2021-02-21 14:23:41 -0600

    Initial UI design

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from "./pages/Home";
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
