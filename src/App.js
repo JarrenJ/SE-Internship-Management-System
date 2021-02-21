4f06e51cc9e5961ee28099b62bfef6a81900a604
JarrenJ <j.jarren@yahoo.com>
2021-02-21 14:40:50 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from "./pages/Home";
-// import './App.css';
+// import './colors.css';
 
 function App() {
   return (
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import '../../App.css';
+import '../../colors.css';
 import './home.css';
 
 
