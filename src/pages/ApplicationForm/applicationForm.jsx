52515c3bf01aed4500d5d6eab303abcbc6ccc1df
Patrick <pat14trani@gmail.com>
2021-02-25 19:44:45 -0600

    Beginning of Application Page
    Beginnings of Application Submission Page done. Spent most time looking into the modular pack and input bars.

--- src/pages/ApplicationForm/applicationForm.jsx
+++ src/pages/ApplicationForm/applicationForm.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import TextField from '@material-ui/core/TextField';
 
-
-
+/*Application Form will load the application page along with the form to fill out.
+Form will be one page and at the bottom will be two buttons. Upon clicking one of those buttons the data submitted will be stored and sent
+*/
 export function ApplicationForm(){
 
-    return(<p>test</p>)
+    return(
+        <div className={"main"}>
+            <form className={'app__form'} >
+                <h1>Application Submission Form</h1>
+                <h4>Student ID</h4>
+                <TextField id="filled-basic" label='Student ID' variant='filled'/>
+
+
+            </form>
+
+
+
+
+
+        </div>
+    )
 }
