180cd38138f8f9ef0b1a63e8c537e8afda90eaa7
Jarren Jackson <j.jarren@yahoo.com>
2021-02-26 17:00:18 -0600

    Login Page -- Base Functionality (#3)
    Basic functionality implemented
    - Query db for valid users
    - Protected routes for authenticated users

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,25 +1,106 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { Link, useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
+import { NWHorizontal2Color } from 'assets'
 import '../../colors.css';
 import './home.css';
 
 export function Home() {
+    const { register, handleSubmit } = useForm();
+    const [message, setMessage] = useState({});
+    const history = useHistory();
+
+    const onSubmit = (data, e) => {
+        console.log(JSON.stringify(data))
+        setMessage({
+            data: "Logging in...",
+        });
+        // Fetch api endpoint to query database for user with credentials from form
+        fetch(`/api/auth`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(data),
+        })
+            .then((res) => {
+                if (res.ok) {
+                    // api returned status 200, so we return the response
+                    console.log('res is ok')
+                    return res.text()
+                } else {
+                    // api returned code 400, so we throw an error to catch later
+                    console.log('res is not ok')
+                    throw new Error('Something went wrong with the /api/auth endpoint (Most likely an invalid user)')
+                }
+            })
+            .then(({ error }) => {
+                setMessage({
+                    data: error || "Logged in successfully, redirecting...",
+                });
+
+                !error &&
+                setTimeout(() => {
+                    // set JWT here - for login session and to guard routes so only authenticated users can access them
+                    localStorage.setItem("token", data.token);
+                    // send user to dashboard
+                    // PUSH new url onto history so create a new entry
+                    history.push("/playground");
+                }, 3000);
+
+                !error && e.target.reset();
+            })
+            // Catch user with invalid credentials here
+            .catch((error) => {
+                console.log(error)
+                setMessage({
+                    data: 'Invalid Credentials',
+                });
+            });
+    };
 
     return(
         <div className="main">
+            <div className="login__logo">
+                <img src={NWHorizontal2Color} alt='NW_Horizontal_2Color'/>
+            </div>
             <div className="login__box">
                 <div className="row">
                     <div className="login__column">
                         <div className="login__img" />
                     </div>
                     <div className="login__column">
-                        <form className="login__form" onSubmit={(e) => e.preventDefault()}>
+                        <form className="login__form" onSubmit={handleSubmit(onSubmit)}>
                             <h2>Northwest Network Account Login</h2>
-                            <input placeholder="Please enter your northwest account username" name="login__username" type="text"/>
-                            <input placeholder="Please enter your northwest account password" name="login__password" type="password"/>
-                            <span><input type="checkbox"/>Remember Me</span>
+                            <input
+                                placeholder="Please enter your northwest account username"
+                                name="username"
+                                type="text"
+                                ref={register({ required: true })}
+                            />
+                            <input
+                                placeholder="Please enter your northwest account password"
+                                name="password"
+                                type="password"
+                                ref={register({ required: true })}
+                            />
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        // checked={checked}
+                                        // onChange={handleChange}
+                                        name="checkedB"
+                                        color="primary"
+                                    />
+                                }
+                                label="Remember me"
+                            />
                             <button type="submit">Submit</button>
+                            <small className='login__error'>{message.data}</small>
                             <span><Link to="/forgotPassword">Forgot Password?</Link> | <Link to="/needHelp">Need Help?</Link></span>
                         </form>
                     </div>
--- src/server/server.js
+++ src/server/server.js
@@ -1,42 +1,83 @@
 const express = require('express');
 const mysql = require('mysql');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
-const app = express();
-const port = 8000;
-const authTable ='users';
-
-// const pool = mysql.createPool({
-//     host: process.env.MYSQL_HOST,
-//     user: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PWD,
-//     database: process.env.MYSQL_DB,
-// });
-
-const db = mysql.createConnection({
+// Create connection to mySQL database
+const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PWD,
     database: process.env.MYSQL_DB,
-});
+})
 
-db.connect(err => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Connected to the MySQL server');
-    }
-});
+const app = express()
 
-app.listen(port, () => {
-    console.log(`App server now listening to port ${port}`);
-});
+app.use(cors())
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
 
 app.get('/api/users', (req, res) => {
-    db.query(`select * from ${authTable}`, (err, rows) => {
+    connection.query(`SELECT * FROM accounts`, (err, rows) => {
         if (err) {
-            res.send(err);
+            res.send(err)
         } else {
-            res.send(rows);
+            res.send(rows)
         }
     });
+});
+
+app.post('/api/auth', (request, response) => {
+    const username = request.body.username
+    const password = request.body.password
+    connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+        console.log('query database...')
+        if (results.length > 0) {
+            console.log('found user!')
+            request.session.loggedin = true
+            request.session.username = username
+
+            const token = jwt.sign(
+                // payload data
+                {
+                    name: request.body.username,
+                },
+                '1234'
+            );
+
+            response.header("auth-token", token).json({
+                error: null,
+                data: {
+                    token,
+                },
+            });
+
+            response.status(200)
+        } else {
+            console.log('Invalid User!')
+            response.status(400)
+        }
+        console.log('ended')
+        response.end();
+    });
+});
+
+// app.get('/home', function(request, response) {
+//     if (request.session.loggedin) {
+//         response.send('Welcome back, ' + request.session.username + '!');
+//     } else {
+//         response.send('Please login to view this page!');
+//     }
+//     response.end();
+// });
+
+app.listen(8000, () => {
+    console.log(`App server now listening to port 8000`);
 });
