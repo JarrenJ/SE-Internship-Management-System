5c02d51657bea58ab8342cd515e545a7a0124a40
Jarren Jackson <j.jarren@yahoo.com>
2021-03-01 16:30:19 -0600

    Conditional rendering for different roles

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -4,14 +4,9 @@ import { NWHorizontal2Color } from 'assets'
 import styled from "styled-components"
 
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
 
 import '../../colors.css'
 import './SideNav.css'
@@ -42,10 +37,8 @@ const StyledMenu = withStyles({
     />
 ));
 
-const StyledMenuItem = withStyles((theme) => ({
+const StyledMenuItem = withStyles(() => ({
     root: {
-        // paddingRight: '3rem',
-        // paddingLeft: '3rem',
         '&:focus': {
             backgroundColor: 'lightgray',
             '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
@@ -56,56 +49,99 @@ const StyledMenuItem = withStyles((theme) => ({
 }))(MenuItem);
 
 
-export function SideNav() {
+export function SideNav({isAdmin, isFaculty, isStudent}) {
     const [anchorEl, setAnchorEl] = useState(null);
+    const [down, setDown] = useState(false)
+    const [navWidth, setNavWidth] = useState('300px')
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
+    const handleClick = (e) => {
+        setDown(!down)
+        setAnchorEl(e.currentTarget);
     };
 
     const handleClose = () => {
         setAnchorEl(null);
+        setDown(!down)
     };
 
+    const closeSideNav = () => {
+        setNavWidth('0px')
+    }
+
+    const openSideNav = () => {
+        setNavWidth('300px')
+    }
     return(
-        <div className='sidenav'>
-            <div className='sidenav__logo'>
-                <img src={NWHorizontal2Color} alt='NW_Horizontal_2Color' />
+        <div className='container'>
+            <div className='sidenav' style={{width: navWidth}}>
+                <div className='sidenav__logo'>
+                    <img src={NWHorizontal2Color} alt='NW_Horizontal_2Color' />
+                </div>
+                <Divider />
+                {!isStudent &&
+                    <>
+                        <div className='sidenav__link'>
+                            <Link to='#'><i className="fas fa-tachometer-alt" /> Dashboard</Link>
+                        </div>
+                        <Divider />
+                    </>
+                }
+                <div className='sidenav__popup'>
+                    {isAdmin &&
+                        <Link
+                            to='#'
+                            aria-controls="customized-menu"
+                            aria-haspopup="true"
+                            variant="contained"
+                            color="primary"
+                            onClick={handleClick}
+                        >
+                            <i className="fas fa-folder" /> Reports {down ? <i className="fas fa-chevron-down left" /> : <i className="fas fa-chevron-right left" /> }
+                        </Link>
+                    }
+                    {isFaculty &&
+                        <Link to='#'>
+                            <i className="fas fa-folder" /> Applications
+                        </Link>
+                    }
+
+                    {isStudent &&
+                        <>
+                            <Link to='#'>
+                                <i className="fas fa-plus-square" /> New Application
+                            </Link>
+                            <Link to='#'>
+                            <i className="fas fa-folder-open" /> Check Status
+                            </Link>
+                        </>
+                    }
+                </div>
+                <div>
+                    <StyledMenu
+                        id="customized-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <StyledMenuItem>
+                            <ListItemText primary="Summary Report" />
+                        </StyledMenuItem>
+                        <StyledMenuItem>
+                            <ListItemText primary="Location Report" />
+                        </StyledMenuItem>
+                        <StyledMenuItem>
+                            <ListItemText primary="Major Report" />
+                        </StyledMenuItem>
+                    </StyledMenu>
+                </div>
+
+                <div className='sidenav__btn__close'>
+                    <button onClick={closeSideNav}><i className="fas fa-chevron-left" /></button>
+                </div>
             </div>
-            <Divider />
-            <div className='sidenav__link'>
-                <Link to='#'><i className="fas fa-tachometer-alt" /> Dashboard</Link>
-            </div>
-            <Divider />
-            <div className='sidenav__popup'>
-                <Link
-                    aria-controls="customized-menu"
-                    aria-haspopup="true"
-                    variant="contained"
-                    color="primary"
-                    onClick={handleClick}
-                >
-                    <i className="fas fa-folder" /> Reports
-                </Link>
-            </div>
-            <div>
-                <StyledMenu
-                    id="customized-menu"
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                >
-                    <StyledMenuItem>
-                        <ListItemText primary="Summary Report" />
-                    </StyledMenuItem>
-                    <StyledMenuItem>
-                        <ListItemText primary="Location Report" />
-                    </StyledMenuItem>
-                    <StyledMenuItem>
-                        <ListItemText primary="Major Report" />
-                    </StyledMenuItem>
-                </StyledMenu>
+            <div className='sidenav__btn__open'>
+                <button onClick={openSideNav}><i className="fas fa-chevron-right" /></button>
             </div>
         </div>
     )
