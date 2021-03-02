bf0597bcef100444e366328b4f438b63ca45687c
AJ-Hagemann <54418965+AJ-Hagemann@users.noreply.github.com>
2021-03-02 15:58:17 -0600

    Setup

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,12 @@
 import NWHorizontal2Color from './NW_HORIZONTAL_2COLOR.png'
 import NWTripleStacked2Color from './NW_TRIPLE-STACKED-2COLOR.png'
+import Manlogo from './Manlogo.png'
+import Hourglass from './Hourglass.png'
+import airplane from './airplane.png'
+import account from './account.png'
+
 export { NWHorizontal2Color, NWTripleStacked2Color }
+export { account }
+export { airplane }
+export { Manlogo }
+export { Hourglass }
--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -1,8 +1,71 @@
 import React from "react";
+import "./dashboard.css"
+import "../../colors.css"
+import { Manlogo } from 'assets'
+import { Hourglass } from 'assets'
+import { airplane } from 'assets'
+import { account } from 'assets'
 
-import { SideNav } from 'components'
+export function Dashboard() {
+    return (
+        <div className="dashboard__container">
+            <div className="dashboard__Header">
+            <img src={ account } alt='account.png'/>
+                <div className="Header_Namebox">
+                    <p>Cindy Tu</p>
+                </div>
+            </div>
+            <p>Dashboard</p>
+            <div className='dashboard__row'>
+                <div className='dashboard__column__4'>
+                    <div className="dashboard__Button1">
+                        <p>Total Interns</p><img src={ Manlogo } alt='Manlogo.png'/><p>4</p>
+                    </div>
+                </div>
+                <div className='dashboard__column__4'>
+                    <div className="dashboard__Button2">
+                        <p>Active Interns</p><img src={ Manlogo } alt='Manlogo.png'/><p>2</p>
+                    </div>
+                </div>
+                <div className='dashboard__column__4'>
+                    <div className="dashboard__Button3">
+                        <p>Pending Approvals</p><img src={ Hourglass } alt='Hourglass.png'/><p>1</p>
+                    </div>
+                </div>
+                <div className='dashboard__column__4'>
+                    <div className="dashboard__Button4">
+                        <p>Out of State</p><img src ={ airplane } alt='airplace.png'/><p>3</p>
+                    </div>
+                </div>
+            </div>
+            <div className='dashboard__Map_header'>
+                <p>Interns Map</p>
+            </div>
+            <div className='dashboard__Map'>
+            </div>
+        </div>
 
-export function Dashboard({role}) {
+    )
+}
+export function StudentView() {
+
+    return (
+        <div>
+            THIS IS THE STUDENT PAGE
+        </div>
+    );
+}
+
+export function AdminView() {
+
+    return (
+        <div>
+            THIS IS THE ADMIN PAGE
+        </div>
+    );
+}
+
+export function FacultyView() {
     return (
         <>
             <SideNav role={role} />
