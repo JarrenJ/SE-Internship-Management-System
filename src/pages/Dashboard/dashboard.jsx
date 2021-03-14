801f323ee2cdd9be442876ef311f95eeaf091cc8
Jarren Jackson <j.jarren@yahoo.com>
2021-03-13 21:55:42 -0600

    Initial Design / structure of 3-step form

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-import { SideNav } from 'components'
+import {ApplicationForm1, SideNav} from 'components'
+
+import './dashboard.css'
 
 export function Dashboard({role}) {
     return (
         <>
             <SideNav role={role} />
             <div className="dashboard__container">
-                <div className="dashboard__Header"></div>
-                <p>Hello world!</p>
+                {/*<div className="dashboard__Header"></div>*/}
+                {/*<p>Hello world!</p>*/}
+                <ApplicationForm1 />
             </div>
         </>
     )
