5df3ff60bf14d0fb2c3ea4ebcbbae9d45b5bb856
Jarren Jackson <j.jarren@yahoo.com>
2021-03-15 18:13:52 -0500

    Modifed container for the 4 cards to use flex and updated trhe Namebox in the header

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -5,21 +5,45 @@ import "../../colors.css"
 export function Dashboard() {
     return (
         <div className="dashboard__container">
-            <div className="dashboard__Header"> <div className="Header_Namebox"> <p>Name</p></div> </div>
+            <div className="dashboard__Header">
+                <div className="Header_Namebox">
+                    <p>Name</p>
+                </div>
+            </div>
             <p>Dashboard</p>
-            <div className="Button_box" >
-            <div className="dashboard__Button1">
-            <p>Total Interns</p> </div>
-            <div className="dashboard__Button2"> 
-            <p>Active Interns</p> </div>
-            <div className="dashboard__Button3"> 
-            <p>Pending Approvals</p> </div>
-            <div className="dashboard__Button4"> 
-            <p>Out of State</p> </div></div>
-            
-            
+            {/*<div className="Button_box" >*/}
+            {/*<div className="dashboard__Button1">*/}
+            {/*<p>Total Interns</p> </div>*/}
+            {/*<div className="dashboard__Button2">*/}
+            {/*<p>Active Interns</p> </div>*/}
+            {/*<div className="dashboard__Button3">*/}
+            {/*<p>Pending Approvals</p> </div>*/}
+            {/*<div className="dashboard__Button4">*/}
+            {/*<p>Out of State</p> </div></div>*/}
+            <div className='dashboard__row'>
+                <div className='dashboard__column__4'>
+                    <div className="dashboard__Button1">
+                        <p>Total Interns</p>
+                    </div>
+                </div>
+                <div className='dashboard__column__4'>
+                    <div className="dashboard__Button2">
+                        <p>Active Interns</p>
+                    </div>
+                </div>
+                <div className='dashboard__column__4'>
+                    <div className="dashboard__Button3">
+                        <p>Pending Approvals</p>
+                    </div>
+                </div>
+                <div className='dashboard__column__4'>
+                    <div className="dashboard__Button4">
+                        <p>Out of State</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        
+
     )
 }
 export function StudentView() {
