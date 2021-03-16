f0ad39f3ed89ea634ad240574a2ce53115548e1f
AJ-Hagemann <54418965+AJ-Hagemann@users.noreply.github.com>
2021-03-15 22:01:35 -0500

    Interns map

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,12 @@
 import NWHorizontal2Color from './NW_HORIZONTAL_2COLOR.png'
+import Manlogo from './Manlogo.png'
+import Hourglass from './Hourglass.png'
+import airplane from './airplane.png'
+import account from './account.png'
 
+export { account }
+export { airplane }
+export { Manlogo }
+export { Hourglass }
 export { NWHorizontal2Color }
+
