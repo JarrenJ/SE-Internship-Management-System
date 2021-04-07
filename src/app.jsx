f22e0374e11fb58013ff7630164c9b29f7d9cce1
Jarren Jackson <j.jarren@yahoo.com>
2021-04-07 16:17:55 -0500

    Changes based on feedback

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import {Home, Dashboard, ApplicationForm} from "pages";
-import {SampleComponent} from "components";
 
 const App = () => {
     // Allow only authenticated users to view a protected route
@@ -19,7 +18,6 @@ const App = () => {
             <Route path='/' exact component={Home} />
             {/* Leaving this in so we have a version of appForm that is single page will most likely end up deleting*/}
             <Route path='/appForm' component={ApplicationForm} />
-            <Route path='/playground' component={authGuard(SampleComponent)} />
             <Route path='/dashboard' component={authGuard(Dashboard)} />
             {/*<Route component={error404} />*/}
         </Switch>
