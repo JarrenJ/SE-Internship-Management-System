7c6f0118153c2f4325d469d95e3815820977d880
Jarren Jackson <j.jarren@yahoo.com>
2021-04-11 16:46:31 -0500

    Changed up import statement order for dashboardPanel

--- src/components/DashboardPanel/DashboardPanel.jsx
+++ src/components/DashboardPanel/DashboardPanel.jsx
@@ -1,20 +1,19 @@
 import React, {useState} from "react";
 import {account, airplane, Hourglass, Manlogo, NWDoubleStackedGreen} from "assets";
 import {ApplicationForm} from "components";
-import styled from "styled-components";
-
-import {DataGrid} from '@material-ui/data-grid';
-
-import './DashboardPanel.css'
-import '../../colors.css'
-
 
 import Button from '@material-ui/core/Button';
+import { DataGrid } from '@material-ui/data-grid';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import styled from "styled-components";
+
+import './DashboardPanel.css'
+import '../../colors.css'
+
 
 const Container = styled.div`
   display: flex;
